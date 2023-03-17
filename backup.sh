11c5965cf7fe2cf425c0c30f557b82d1c7b348ed
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-03-17 15:16:19 +0900

    パスの微修正

--- backup.sh
+++ backup.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 ##設定##
-STARTER="/root/minecraft-server/start.sh" #サーバの起動スクリプトのパス
-SCREEN="minecraft"                        #minecraftを実行しているscreen名
+STARTER="/home/NX-EN/minecraft-server/start.sh" #サーバの起動スクリプトのパス
+SCREEN="minecraft"                              #minecraftを実行しているscreen名
 
-MC_DIR="/root/minecraft-server"     #バックアップしたいディレクトリのパス
-BU_DIR="/root/MCServer-AutoBackup"  #このスクリプトがあるディレクトリのパス
+MC_DIR="/home/NX-EN/minecraft-server"     #バックアップしたいディレクトリのパス
+BU_DIR="/home/NX-EN/MCServer-AutoBackup"  #このスクリプトがあるディレクトリのパス
 
 BU_TIME=$(date +%Y%m%d-%H%M%S)  #コミットメッセージに入れる日時のフォーマット
 BU_NAME="AutoBackup_${BU_TIME}" #コミットメッセージの名称
 
 CNT1=300                    #サーバを自動停止する300秒前にお知らせ
-CNT2=$(expr $CNT1 - $CNT3)  #サーバが自動停止する30秒前になるまで(270秒間)待機
-CNT3=30                     #サーバが自動停止する30秒前にもう一度お知らせ
-CNT4=120                    #サーバが完全に停止するまで120秒待機
+CNT2=30                     #サーバが自動停止する30秒前にもう一度お知らせ
+CNT3=120                    #サーバが完全に停止するまで120秒待機
 ########
 
 
@@ -23,12 +22,12 @@ screen -r $SCREEN -X stuff "say §9§lお知らせ\015"
 echo -e "\e[36m[ MCServer-AutoBackup ] Waiting $CNT1 sec...\e[m"
 screen -r $SCREEN -X stuff "say §3§l§o"$CNT1"§r§6秒後に§3§lバックアップ§r§6及び§3§l再起動§r§6を行います\015"
 screen -r $SCREEN -X stuff "say §6セーブしてサーバから退出してください\015"
+sleep $(expr $CNT1 - $CNT2)
+screen -r $SCREEN -X stuff "say §c§l§o"$CNT2"§r§4§l秒後にサーバーが自動停止します\015"
 sleep $CNT2
-screen -r $SCREEN -X stuff "say §c§l§o"$CNT3"§r§4§l秒後にサーバーが自動停止します\015"
-sleep $CNT3
 echo -e "\e[36m[ MCServer-AutoBackup ] Stopping minecraft server...\e[m"
 screen -r $SCREEN -X stuff "stop\015"
-sleep $CNT4
+sleep $CNT3
 
 ##サーバデータを同期する##
 echo -e "\e[36m[ MCServer-AutoBackup ] Syncing minecraft-server data...\e[m"
